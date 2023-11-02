7ea28677fc6b05b02f5aefc0fdde124956ed680e
Sean Lenard B <cordysean@163.com>
2023-11-02 21:11:53 +0800

    remove old extensions

--- Program.cs
+++ Program.cs
@@ -2,13 +2,14 @@
 using MagicShaper.AdfExtensions;
 using MagicShaper.AdofaiCore.AdfClass;
 using MagicShaper.AdofaiCore.AdfEvents;
+using MagicShaper.VfxProjects;
 using System.Text.Json.Nodes;
 
 internal class Program
 {
 	private static void Main(string[] args)
 	{
-
+		AdfVfxProj_IGotNoTime.Main();
 		
 		
 		
---
+++ VfxProjects/AdfVfxProj_IGotNoTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicShaper.VfxProjects
+{
+	internal class AdfVfxProj_IGotNoTime
+	{
+		public static void Main()
+		{
+
+		}
+
+
+	}
+}
