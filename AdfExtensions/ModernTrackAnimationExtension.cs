09ba30ab19b5558addafc9b7b7df7712ef5a4308
Sean Lenard B. <cordysean@163.com>
2025-03-30 16:06:28 +0800

    1

--- AdfExtensions/ModernTrackAnimationExtension.cs
+++ AdfExtensions/ModernTrackAnimationExtension.cs
@@ -21,8 +21,9 @@ namespace MagicShaper.AdfExtensions
 			double smin = 75, double smax = 200,
 			double angleOffsetVariationProportion = 30d,
 			double endOpacity = 100d,
-			double endScale = 100d)
-			=> chart.ModernTrackAppear(tileStart, tileEnd, duration, previewBeats, new() { AdfEaseType.OutExpo }, xmin, xmax, ymin, ymax, rmin, rmax, smin, smax, angleOffsetVariationProportion, endOpacity, endScale);
+			double endScale = 100d,
+			double opacityTimeFactor = 1d)
+			=> chart.ModernTrackAppear(tileStart, tileEnd, duration, previewBeats, new() { AdfEaseType.OutExpo }, xmin, xmax, ymin, ymax, rmin, rmax, smin, smax, angleOffsetVariationProportion, endOpacity, endScale, opacityTimeFactor);
 
 		public static void ModernTrackAppear(
 			this AdfChart chart,
@@ -36,7 +37,8 @@ namespace MagicShaper.AdfExtensions
 			double smin = 75, double smax = 200,
 			double angleOffsetVariationProportion = 30d,
 			double endOpacity = 100d,
-			double endScale = 100d)
+			double endScale = 100d,
+			double opacityTimeFactor = 1d)
 		{
 			double standardBpm = chart.GetTileBpmAt(0);
 
@@ -55,7 +57,21 @@ namespace MagicShaper.AdfExtensions
 						Duration = 0d,
 						AngleOffset = -1145140d
 					});
-				chart.ChartTiles[i].TileEvents.Add(
+				AdfEaseType ease = random.RandIn(easeTypes);
+				double rand = random.NextDouble();
+				chart.ChartTiles[i].TileEvents.Add( // Opacity
+					new AdfEventMoveTrack()
+					{
+						StartTile = new(i, AdfTileReferenceType.Start),
+						EndTile = new(i, AdfTileReferenceType.Start),
+						Opacity = endOpacity,
+						Ease = AdfEaseType.Linear,
+						Duration = opacityTimeFactor * chart.GetTileBpmAt(i) / standardBpm * duration,
+						AngleOffset =
+							opacityTimeFactor * (-chart.GetTileBpmAt(i) / standardBpm * (previewBeats + duration) * 180)
+							* (1 - rand * angleOffsetVariationProportion / 100d)
+					});
+				chart.ChartTiles[i].TileEvents.Add( // Position
 					new AdfEventMoveTrack()
 					{
 						StartTile = new(i, AdfTileReferenceType.Start),
@@ -63,16 +79,28 @@ namespace MagicShaper.AdfExtensions
 						PositionOffset = new(0d, 0d),
 						RotationOffset = 0d,
 						Scale = new(endScale),
-						Opacity = endOpacity,
-						Ease = random.RandIn(easeTypes),
+						Ease = ease,
 						Duration = chart.GetTileBpmAt(i) / standardBpm * duration,
 						AngleOffset =
 							(-chart.GetTileBpmAt(i) / standardBpm * (previewBeats + duration) * 180)
-							* (1 - random.NextDouble() * angleOffsetVariationProportion / 100d)
+							* (1 - rand * angleOffsetVariationProportion / 100d)
 					});
 
 			}
 		}
+
+		public static void ModernTrackDisappear(
+			this AdfChart chart,
+			int tileStart,
+			int tileEnd,
+			double duration,
+			double postviewBeats,
+			double xmin = -10, double xmax = 10, double ymin = -20, double ymax = -10,
+			double rmin = -45, double rmax = 45,
+			double smin = 75, double smax = 200,
+			double angleOffsetVariationProportion = 30d,
+			double opacityTimeFactor = 1d)
+			=> chart.ModernTrackDisappear(tileStart, tileEnd, duration, postviewBeats, new() { AdfEaseType.InExpo }, xmin, xmax, ymin, ymax, rmin, rmax, smin, smax, angleOffsetVariationProportion, opacityTimeFactor);
 
 		public static void ModernTrackDisappear(
 			this AdfChart chart,
@@ -84,14 +112,29 @@ namespace MagicShaper.AdfExtensions
 			double xmin = -10, double xmax = 10, double ymin = -20, double ymax = -10,
 			double rmin = -45, double rmax = 45,
 			double smin = 75, double smax = 200,
-			double angleOffsetVariationProportion = 30d)
+			double angleOffsetVariationProportion = 30d,
+			double opacityTimeFactor = 1d)
 		{
 			double standardBpm = chart.GetTileBpmAt(0);
 
 			Random random = new();
 			for (int i = tileStart; i < tileEnd; i++)
 			{
-				chart.ChartTiles[i].TileEvents.Add(
+				AdfEaseType ease = random.RandIn(easeTypes);
+				double rand = random.NextDouble();
+				chart.ChartTiles[i].TileEvents.Add( // Opacity
+					new AdfEventMoveTrack()
+					{
+						StartTile = new(i, AdfTileReferenceType.Start),
+						EndTile = new(i, AdfTileReferenceType.Start),
+						Opacity = 0d,
+						Duration = opacityTimeFactor * chart.GetTileBpmAt(i) / standardBpm * duration,
+						AngleOffset =
+							opacityTimeFactor * (chart.GetTileBpmAt(i) / standardBpm * (postviewBeats + duration) * 180)
+							* (1 - rand * angleOffsetVariationProportion / 100d),
+						Ease = AdfEaseType.Linear
+					});
+				chart.ChartTiles[i].TileEvents.Add( // Position
 					new AdfEventMoveTrack()
 					{
 						StartTile = new(i, AdfTileReferenceType.Start),
@@ -99,26 +142,14 @@ namespace MagicShaper.AdfExtensions
 						PositionOffset = new(random.RandBetween(xmin, xmax), random.RandBetween(ymin, ymax)),
 						RotationOffset = random.RandBetween(rmin, rmax),
 						Scale = new(random.RandBetween(smin, smax)),
-						Opacity = 0d,
 						Duration = chart.GetTileBpmAt(i) / standardBpm * duration,
 						AngleOffset =
 							(chart.GetTileBpmAt(i) / standardBpm * (postviewBeats + duration) * 180)
-							* (1 - random.NextDouble() * angleOffsetVariationProportion / 100d),
-						Ease = random.RandIn(easeTypes)
+							* (1 - rand * angleOffsetVariationProportion / 100d),
+						Ease = ease
 					});
 			}
 		}
 
-		public static void ModernTrackDisappear(
-			this AdfChart chart,
-			int tileStart,
-			int tileEnd,
-			double duration,
-			double postviewBeats,
-			double xmin = -10, double xmax = 10, double ymin = -20, double ymax = -10,
-			double rmin = -45, double rmax = 45,
-			double smin = 75, double smax = 200,
-			double angleOffsetVariationProportion = 30d)
-			=> chart.ModernTrackDisappear(tileStart, tileEnd, duration, postviewBeats, new() { AdfEaseType.InExpo }, xmin, xmax, ymin, ymax, rmin, rmax, smin, smax, angleOffsetVariationProportion);
 	}
 }
--- VfxProjects/AdfVfxProj_BydReaches.cs
+++ VfxProjects/AdfVfxProj_BydReaches.cs
@@ -19,9 +19,63 @@ namespace MagicShaper.VfxProjects
 			chart.ModernTrackDisappear(0, 117, 4d, -2d, -2, -0.5, -1.5, -0.5);
 
 			chart.ModernTrackAppear(117, 740, 8d, 12d, -3, 3, -2, 2, endOpacity: 1000);
-			chart.ModernTrackDisappear(117, 740, 8d, -6d, -3, 3, -2, 2);
+			chart.ModernTrackDisappear(117, 740, 4d, -4d, -3, 3, -2, 2, smax: 75, smin: 25, opacityTimeFactor: 0.8);
 
-			File.WriteAllText(@"G:\Adofai levels\BYD Reaches\level-eff.adofai", chart.ChartJson.ToJsonString());
+			chart.ModernTrackAppear(740, 1038, 4d, 8d, 0.5, 2, 0.5, 1.5, endOpacity: 1000);
+			chart.ModernTrackDisappear(740, 1038, 4d, 0d, -2, -0.5, -0.5, 0.5);
+
+			chart.ModernTrackAppear(1038, 1703, 8d, 12d, -3, 3, -2, 2, endOpacity: 1000);
+			chart.ModernTrackDisappear(1038, 1703, 4d, -4d, -3, 3, -2, 2, smax: 75, smin: 25, opacityTimeFactor: 0.8);
+
+			chart.ModernTrackAppear(1703, 1936,  4d, 8d, 0.5, 2, 0.5, 1.5, endOpacity: 1000);
+			chart.ModernTrackDisappear(1703, 1936, 4d, -2d, -2, -0.5, -1.5, -0.5);
+
+			chart.ModernTrackAppear(1936, 3261, 8d, 12d, -3, 3, -2, 2, endOpacity: 1000);
+			chart.ModernTrackDisappear(1936, 3261, 4d, -4d, -3, 3, -2, 2, smax: 75, smin: 25, opacityTimeFactor: 0.8);
+
+
+
+			var builder = chart.JudgementLimit()
+				.SetDisplayType(JudgementLimitDisplayType.Images)
+				.SetImages("j4.png", "j3.png", "j2.png", "j1.png")
+				.GetBuilder();
+
+			builder.Limit(7, JudgementLimitWindowType.Normal);
+			builder.Limit(88, JudgementLimitWindowType.Perfects);
+			builder.Limit(341, JudgementLimitWindowType.NoMiss);
+			builder.Limit(491, JudgementLimitWindowType.Perfects);
+			builder.Limit(740, JudgementLimitWindowType.PurePerfect);
+			builder.Limit(936, JudgementLimitWindowType.Perfects);
+			builder.Limit(1038, JudgementLimitWindowType.NoMiss);
+			builder.Limit(1118, JudgementLimitWindowType.Perfects);
+			builder.Limit(1214, JudgementLimitWindowType.NoMiss);
+			builder.Limit(1415, JudgementLimitWindowType.Perfects);
+			builder.Limit(1585, JudgementLimitWindowType.NoMiss);
+			builder.Limit(1657, JudgementLimitWindowType.Perfects);
+			builder.Limit(1703, JudgementLimitWindowType.Normal);
+			builder.Limit(1823, JudgementLimitWindowType.Perfects);
+			builder.Limit(1887, JudgementLimitWindowType.NoMiss);
+			builder.Limit(2018, JudgementLimitWindowType.Perfects);
+			builder.Limit(2078, JudgementLimitWindowType.NoMiss);
+			builder.Limit(2447, JudgementLimitWindowType.Perfects);
+			builder.Limit(2611, JudgementLimitWindowType.PurePerfect);
+			builder.Limit(2721, JudgementLimitWindowType.NoMiss);
+			builder.Limit(2898, JudgementLimitWindowType.PurePerfect);
+			builder.Limit(2990, JudgementLimitWindowType.Perfects);
+			builder.Limit(3131, JudgementLimitWindowType.PurePerfect);
+			builder.Limit(3260, JudgementLimitWindowType.Normal);
+
+
+			builder.BuildDisplay(0, 1960, 100, 8);
+
+			File.WriteAllText(@"G:\Adofai levels\BYD Reaches\level-eff-100Judgement.adofai", chart.ChartJson.ToJsonString());
+
+			builder.BuildLimit();
+
+
+
+
+			File.WriteAllText(@"G:\Adofai levels\BYD Reaches\level-eff-judgementlimit.adofai", chart.ChartJson.ToJsonString());
 		}
 
 	}
